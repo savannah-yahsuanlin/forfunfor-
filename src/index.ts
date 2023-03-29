18872395354e692c7ceec1e1f6a8ccd5087c3c5c
savannah-yahsuanlin <savannahwang75@gmail.com>
2023-03-29 18:11:01 +0800

    addfunction

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import {v4 as uuidV4} from "uuid"
 
-import confetti from 'canvas-confetti';
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+const list = document.querySelector<HTMLUListElement>('#list')
+const form = document.querySelector<HTMLFormElement>('new-task-form')
+const input = document.querySelector<HTMLInputElement> ('#new-task-title')
+
+
+form?.addEventListener('submit', (e) => {
+	e.preventDefault();
+	if(input?.value === undefined || input?.value === '') return   
+
+	const task = {
+		id: uuidV4(),
+		title: input.value,
+		completed: false,
+		createdAt: new Date()
+	}
+	
+})
