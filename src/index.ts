2949bbfaf3b0af3cd4bb31cdacde1097a8e51ee1
savannah-yahsuanlin <savannahwang75@gmail.com>
2023-03-29 18:20:29 +0800

    additem

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import {v4 as uuidV4} from "uuid"
 
+type Task = {id: string, title: string, completed: boolean, createdAt: Date}
 
 const list = document.querySelector<HTMLUListElement>('#list')
-const form = document.querySelector<HTMLFormElement>('new-task-form')
+const form = document.querySelector<HTMLFormElement>('#new-task-form')
 const input = document.querySelector<HTMLInputElement> ('#new-task-title')
 
 
@@ -10,11 +11,21 @@ form?.addEventListener('submit', (e) => {
 	e.preventDefault();
 	if(input?.value === undefined || input?.value === '') return   
 
-	const task = {
+	const newTask: Task = {
 		id: uuidV4(),
 		title: input.value,
 		completed: false,
 		createdAt: new Date()
 	}
-	
+	addListItem(newTask)
 })
+
+function addListItem(task: Task) {
+	const item = document.createElement('li')
+	const label = document.createElement('label')
+	const checkbox = document.createElement('input')
+	checkbox.type = 'checkbox' 
+	label.append(checkbox, task.title)
+	item.append(label)
+	list?.append(item)
+}
