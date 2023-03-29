b234120ceb85bc81f26bb2eaef598585d651f96c
savannah-yahsuanlin <savannahwang75@gmail.com>
2023-03-29 18:32:17 +0800

    localstorage

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ type Task = {id: string, title: string, completed: boolean, createdAt: Date}
 const list = document.querySelector<HTMLUListElement>('#list')
 const form = document.querySelector<HTMLFormElement>('#new-task-form')
 const input = document.querySelector<HTMLInputElement> ('#new-task-title')
+const tasks: Task[] = loadTasks()
+tasks.forEach(addListItem)
 
 
 form?.addEventListener('submit', (e) => {
@@ -17,6 +19,9 @@ form?.addEventListener('submit', (e) => {
 		completed: false,
 		createdAt: new Date()
 	}
+
+	tasks.push(newTask);
+	saveTasks()
 	addListItem(newTask)
 	input.value = "" 
 })
@@ -25,9 +30,23 @@ function addListItem(task: Task) {
 	const item = document.createElement('li')
 	const label = document.createElement('label')
 	const checkbox = document.createElement('input')
+	checkbox.addEventListener('change', () => {
+		task.completed = checkbox.checked
+		saveTasks()
+	})
 	checkbox.type = 'checkbox' 
 	checkbox.checked = task.completed
 	label.append(checkbox, task.title)
 	item.append(label)
 	list?.append(item)
+}
+
+function saveTasks() {
+	 localStorage.setItem('TASKS', JSON.stringify(tasks ))
+}
+
+function loadTasks():Task[] {
+	const response = localStorage.getItem('TASKS')
+	if(response === null) return []
+	return JSON.parse(response)
 }
